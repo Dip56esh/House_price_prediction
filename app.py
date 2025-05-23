9d0e98ab372ca918908d514d0291c6eef77c6618
Dipesh Dhakal <140264306+Dip56esh@users.noreply.github.com>
2025-05-23 17:09:05 +0545

    Updated

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 import streamlit as st
 import numpy as np
 import pickle
-from sklearn.datasets import fetch_california_housing
-from xgboost import XGBRegressor
+# from sklearn.datasets import fetch_california_housing
+# from xgboost import XGBRegressor
 
 
 filename = "house_prediction_model.pkl"
@@ -16,12 +16,10 @@ st.write("Enter the details below to predict the house price:")
 # Input fields for user
 median_income = st.number_input("Median Income (in $10,000s)", min_value=0.0, step=0.1)
 house_age = st.number_input("House Age (in years)", min_value=0.0, step=0.1)
-avg_rooms = st.number_input("Average Rooms per Household", min_value=0.0, step=0.1)
-avg_bedrooms = st.number_input("Average Bedrooms per Household", min_value=0.0, step=0.1)
+avg_rooms = st.number_input("Number of Rooms per Household", min_value=0.0, step=0.1)
+avg_bedrooms = st.number_input("Number of Bedrooms per Household", min_value=0.0, step=0.1)
 population = st.number_input("Population in Block", min_value=0.0, step=1.0)
 households = st.number_input("Households in Block", min_value=0.0, step=1.0)
-# latitude = st.number_input("Latitude", min_value=-90.0, max_value=90.0, step=0.1)
-# longitude = st.number_input("Longitude", min_value=-180.0, max_value=180.0, step=0.1)
 latitude= 37.85
 longitude= -122.25
 
